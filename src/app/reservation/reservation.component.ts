8fd4c3a07e575d1fe5bc7e08744ad1c5be308a36
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2023-11-25 10:44:46 +0100

    Datapicker added to ReservationForm

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -10,19 +11,37 @@ export class ReservationComponent implements OnInit{
 
   reservationForm!: FormGroup;
 
+  occasions: any = ['Eat & Chill', 'Birthday', 'Annivarsary'];
+  numbers: any = ['1', '2', '3', '4', '5'];
+
+  // maxDate: any = '2024-01-01';
+
+  currentDate: any = new Date();
+  currentTime: any = this.currentDate.getHours() + ":" + this.currentDate.getMinutes() + ":" + this.currentDate.getSeconds();
+
+
   ngOnInit() {
     this.reservationForm = new FormGroup({
-      'name': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'phone': new FormControl(null, Validators.required),
-      'date': new FormControl(null, Validators.required),
-      'number': new FormControl(null, Validators.required),
-      'occasion': new FormControl(null, Validators.required),
-      'message': new FormControl(null, Validators.required)
+      'clientData': new FormGroup({
+        'name': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'phone': new FormControl(null, [Validators.required, Validators.pattern('[0-9]{9}')])
+      }),
+      'reservationDetails': new FormGroup({
+        'date': new FormControl(null, Validators.required),
+        'numbers': new FormControl(null, Validators.required),
+        'occasions': new FormControl(null, Validators.required),
+        'message': new FormControl(null)
+      })
+
     });
   }
 
+
+
   onSubmit() {
-    console.log(this.reservationForm);
+    //dodać reset
   }
+
+ 
 }
