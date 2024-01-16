dd6f100eb82948fa5748b0f2efe2fca55eafa1af
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-16 23:18:52 +0100

    Unit tests added

--- src/app/faq/faq.component.spec.ts
+++ src/app/faq/faq.component.spec.ts
@@ -1,21 +1,35 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FaqComponent } from './faq.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
-// import { FaqComponent } from './faq.component';
+describe('FaqComponent', () => {
+  let component: FaqComponent;
+  let fixture: ComponentFixture<FaqComponent>;
+  let router: Router;
 
-// describe('FaqComponent', () => {
-//   let component: FaqComponent;
-//   let fixture: ComponentFixture<FaqComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FaqComponent, NavbarComponent, FooterComponent],
+      imports: [RouterTestingModule]
+    });
+    fixture = TestBed.createComponent(FaqComponent);
+    component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    fixture.detectChanges();
+  });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [FaqComponent]
-//     });
-//     fixture = TestBed.createComponent(FaqComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  it('should create the faq component', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should navigate to contact page when goToContact method is called', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+
+    component.goToContact();
+    expect(navigateSpy).toHaveBeenCalledWith(['/contact']);
+  })
+});
