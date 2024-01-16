27aa22ad3adb7ae54ca78542d2d2da0418663b30
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-16 23:30:42 +0100

    Unit tests added

--- src/app/error/error.component.spec.ts
+++ src/app/error/error.component.spec.ts
@@ -1,21 +1,35 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ErrorComponent } from './error.component';
+import { ErrorComponent } from './error.component';
+import { AppRoutingModule, routingComponents } from '../app-routing.module';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
-// describe('ErrorComponent', () => {
-//   let component: ErrorComponent;
-//   let fixture: ComponentFixture<ErrorComponent>;
+describe('ErrorComponent', () => {
+  let component: ErrorComponent;
+  let fixture: ComponentFixture<ErrorComponent>;
+  let router: Router;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ErrorComponent]
-//     });
-//     fixture = TestBed.createComponent(ErrorComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppRoutingModule],
+      declarations: [ErrorComponent, routingComponents]
+    });
+    fixture = TestBed.createComponent(ErrorComponent);
+    component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create the error component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call goHome method when the button is clicked', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+
+    component.goHome();
+    expect(navigateSpy).toHaveBeenCalledWith(['']);
+  })
+});
+
