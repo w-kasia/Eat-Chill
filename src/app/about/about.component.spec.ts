645cdb01ff778866b7ed4b2444e1b0fa10afefd7
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-17 10:35:15 +0100

    Unit tests added

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,38 +1,35 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { AboutComponent } from './about.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { BrowserModule } from '@angular/platform-browser';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AboutComponent } from './about.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
 
-// describe('AboutComponent', () => {
-//   let component: AboutComponent;
-//   let fixture: ComponentFixture<AboutComponent>;
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [NgbModule, BrowserModule],
-//       declarations: [AboutComponent]
-//     });
-//     fixture = TestBed.createComponent(AboutComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NgbModule, BrowserModule],
+      declarations: [AboutComponent]
+    });
+    fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create the about component', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should create the about component', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should render all paragraphs, headings and image', () => {
-//     let paragraphs = fixture.nativeElement.querySelector('p');
-//     let heading = fixture.nativeElement.querySelector('h1');
-//     let subheading = fixture.nativeElement.querySelector('h2');
-//     let image = fixture.nativeElement.querySelector('img');
+  it('should render all paragraphs, headings and image', () => {
+    let paragraphs = fixture.nativeElement.querySelectorAll('p');
+    let heading = fixture.nativeElement.querySelector('h1');
+    let subheading = fixture.nativeElement.querySelector('h2');
+    let image = fixture.nativeElement.querySelector('img');
 
-//     expect(paragraphs.length).toBeGreaterThan(0);
-//     for(const paragraph of paragraphs) {
-//       expect(paragraph.textContent).toBeTruthy();
-//     }
-//     expect(heading).toBeTruthy();
-//     expect(subheading).toBeTruthy();
-//     expect(image).toBeTruthy();
-//   });
-// });
+    expect(paragraphs.length).toBe(3);
+    expect(heading).toBeTruthy();
+    expect(subheading).toBeTruthy();
+    expect(image).toBeTruthy();
+  });
+});
