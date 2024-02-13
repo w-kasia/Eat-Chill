35cd44ea71d8118dd9021c84b1e5bbec56c44a36
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-02-13 15:30:29 +0100

    handling error implemented

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -9,12 +9,17 @@ import { HttpClient } from '@angular/common/http';
 export class TestimonialsComponent implements OnInit {
 
   testimonials: any[] = [];
+  errorMessage: string = '';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<any[]>('./assets/testimonials.json').subscribe(data => {
+    this.http.get<any[]>('./assets/testimonialss.json').subscribe(data => {
       this.testimonials = data;
+    },
+    error => {
+        console.log('Error fetchnig json data in Testimonials Component');
+        this.errorMessage = 'Sorry, we can not display testimonials now. Try again later.'
     });
   }
 }
